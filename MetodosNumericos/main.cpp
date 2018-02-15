d186c465cec96d1d1c079c7d46a4853a47e05da5
Osmar Hernández <osmarhernandezs@gmail.com>
2018-02-14 23:57:14 -0600

    Menu: A basic menu has been added.

--- MetodosNumericos/main.cpp
+++ MetodosNumericos/main.cpp
@@ -9,8 +9,53 @@
 #include <iostream>
 using namespace std;
 
+bool exit(bool run) {
+    cout << "Salir\n";
+    return !run;
+}
+
+int getInput() {
+    int choice;
+    cin >> choice;
+    return choice;
+}
+
+void DisplayMenu() {
+    cout << "Selecciona mediante el numero del índice\n";
+    cout << "alguna de las siguientes opciones:\n";
+    cout << "1. Regla del Trapecio\n";
+    cout << "2. Simpson 1/3\n";
+    cout << "3. Simpson 3/8\n";
+    cout << "4. Salir\n";
+    cout << "========================================\n";
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    cout << "Hello, World!\n";
-    return 0;
+    bool run = true;
+    int choice = 0;
+    
+    while (run) {
+        DisplayMenu();
+        choice = getInput();
+        
+        switch (choice) {
+            case 1:
+                cout << "Regla del Trapecio\n";
+                break;
+            case 2:
+                cout << "Simpson 1/3\n";
+                break;
+            case 3:
+                cout << "Simpson 3/8\n";
+                break;
+            case 4:
+                run = exit(run);
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    return EXIT_SUCCESS;
 }
