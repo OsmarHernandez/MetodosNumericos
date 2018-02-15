1a80bb15a5525e8b02ca699e00e87c7ff5783704
Osmar Hernández <osmarhernandezs@gmail.com>
2018-02-14 22:47:06 -0600

    Initial Commit

---
+++ MetodosNumericos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MetodosNumericos
+//
+//  Created by Osmar Hernández on 14/02/18.
+//  Copyright © 2018 itesm. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
